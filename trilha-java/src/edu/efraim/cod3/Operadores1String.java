ebd3c57280260935ff6e4c02fc66ccab0fde730e
Efraim Rocha <95988659+Efrals@users.noreply.github.com>
2024-09-22 19:23:23 -0300

    operadores

---
+++ trilha-java/src/edu/efraim/cod3/Operadores1String.java
@@ -0,0 +1,9 @@
+package edu.efraim.cod3;
+
+public class Operadores1String {
+    public static void main(String[] args) {
+        String nomecompleto = "Nome1 " + "Completo2";
+
+        System.out.println(nomecompleto);
+    }
+}
