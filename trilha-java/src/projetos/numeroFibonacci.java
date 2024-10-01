99e05dd01b89128433e58efa48df3675df77f784
Efraim Rocha <95988659+Efrals@users.noreply.github.com>
2024-10-01 16:51:37 -0300

    inserido controle de Fluxo e projeto Fibonacci

---
+++ trilha-java/src/projetos/numeroFibonacci.java
@@ -0,0 +1,36 @@
+package projetos;
+import java.util.Scanner;
+
+public class numeroFibonacci {
+
+    public static void main(String[] args) {
+        // Criação do scanner para entrada do usuário
+        Scanner scanner = new Scanner(System.in);
+
+        // Solicitando um número ao usuário
+        System.out.print("Informe um número: ");
+        int numero = scanner.nextInt();
+
+        // Verificando se o número informado está na sequência de Fibonacci
+        if (fibonacci(numero))
+        {
+            System.out.println("O número " + numero + " pertence à sequência de Fibonacci.");
+        } else {
+            System.out.println("O número " + numero + " Não pertence à sequência de Fibonacci.");
+        }
+    }
+
+    // Método para verificar se o número pertence à sequência de Fibonacci
+    public static boolean fibonacci(int numeroInserido) {
+        if (numeroInserido < 0) return false;
+        
+        int a = 0, b = 1;
+        while (a < numeroInserido) {
+            int temporario = a;
+            a = b;
+            b = temporario + b;
+        }
+        return a == numeroInserido;
+    }
+
+}
